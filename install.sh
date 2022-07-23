55bd62d7853256bae4c1177ba13e813c1807402b
Lee Jordan <duracell80@users.noreply.github.com>
2022-07-23 11:48:44 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+cp -r ./ ~/.themes
+cd ~/
+git clone https://github.com/wmk69/Color-Icons
+cd ~/Color-Icons
+tar -xvzf Color-Icons.tar.gz
+cp -r Color-Icons/White-Icons ~/.local/share/icons
+cp -r Color-Icons/Black-Icons ~/.local/share/icons
