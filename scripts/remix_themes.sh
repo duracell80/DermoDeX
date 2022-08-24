9b45e5ac39e3da30d5c496565b8c8b73cc5200a4
Lee Jordan <duracell80@users.noreply.github.com>
2022-08-24 00:52:03 -0500

    Tweaks and Deps mint21

--- scripts/remix_themes.sh
+++ scripts/remix_themes.sh
@@ -6,7 +6,20 @@ CWD=$(pwd)
 LWD=$HOME/.local/share/dermodex
 
 # APPEND CINNAMON-EXT TO CINNAMON
-cat $LWD/cinnamon-ext.css >> $LWD/theme/cinnamon/cinnamon.css
+rm -rf $LWD/theme/cinnamon/cinnamon_base.css
+
+cp $LWD/theme/cinnamon/cinnamon.css $LWD/theme/cinnamon/cinnamon.orig
+cp $LWD/theme/cinnamon/cinnamon.css $LWD/theme/cinnamon/cinnamon_base.css
+
+chmod u+rw $LWD/theme/cinnamon/cinnamon_base.css
+
+cat $LWD/cinnamon-ext.css >> $LWD/theme/cinnamon/cinnamon_base.css
+cp -f $LWD/theme/cinnamon/cinnamon_base.css $LWD/theme/cinnamon/cinnamon.css
+
 
 # COPY HYBRID AS DERMODEX
 cp -rf $LWD/theme/* $HOME/.themes/DermoDeX
+cp -f $LWD/theme/cinnamon/cinnamon.orig $LWD/theme/cinnamon/cinnamon.css
+
+
+xdotool key ctrl+alt+"Escape"
